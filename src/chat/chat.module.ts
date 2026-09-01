6f5067c4e4b75ef52151fbbd6116e8cd87905cb1
Jesus F <sherman589@gmail.com>
2026-09-01 16:35:34 -0400

    fix(deploy): romper dependencia circular ChatModule <-> EquipoModule con forwardRef()

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { EquipoModule } from '../equipo/equipo.module';
 import { ChatController } from './chat.controller';
@@ -6,7 +6,7 @@ import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 
 @Module({
-  imports: [AuthModule, EquipoModule],
+  imports: [AuthModule, forwardRef(() => EquipoModule)],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
   exports: [ChatService, ChatGateway],
--- src/equipo/equipo.module.ts
+++ src/equipo/equipo.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { EquipoController } from './equipo.controller';
 import { EquipoService } from './equipo.service';
 import { ChatModule } from '../chat/chat.module';
 import { NotificacionesModule } from '../notificaciones/notificaciones.module';
 
 @Module({
-  imports: [ChatModule, NotificacionesModule],
+  imports: [forwardRef(() => ChatModule), NotificacionesModule],
   controllers: [EquipoController],
   providers: [EquipoService],
   exports: [EquipoService],
